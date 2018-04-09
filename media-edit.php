76fd60429cc9e4c0c1d48bc5d7a751a4751b5cf0
Catalin Manea <catalinmanea@c-it.ro>
2018-04-09 23:58:41 +0300

    aa

---
+++ media-edit.php
@@ -0,0 +1,136 @@
+<?php
+include 'header.php';
+?>
+
+
+
+<!-- Page wrapper  -->
+<!-- ============================================================== -->
+<div class="page-wrapper" style="min-height: 928px;">
+    <!-- ============================================================== -->
+    <!-- Container fluid  -->
+    <!-- ============================================================== -->
+    <div class="container-fluid">
+        <!-- ============================================================== -->
+        <!-- Bread crumb and right sidebar toggle -->
+        <!-- ============================================================== -->
+
+        <div class="row page-titles">
+            <div class="col-md-5 text-left">
+                <h2 class="text-themecolor">MEDIA-Edit</h2>
+            </div>
+        </div>
+
+
+        <div class="row">
+            <div class="col-lg-12 col-md-12">
+                <div class="row whitebg roundcorners" >
+
+
+                    <div class="col-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <!--                        <h4 class="card-title">Contact Emplyee list</h4>-->
+                                <!--                        <h6 class="card-subtitle"></h6>-->
+                                <div class="col-7">
+
+
+
+                                    <form class="form">
+                                        <div class="form-group row">
+                                            <div class="col-10">
+                                                <h5 class="">CAPTION</h5>
+                                                <input class="form-control" type="text" value="Text here" id="example-text-input">
+                                            </div>
+                                        </div>
+
+                                        <div class="form-group row">
+                                            <div class="col-10">
+                                                <h5 class="">CAPTION</h5>
+                                                <select class="custom-select" id="inlineFormCustomSelect">
+                                                    <option selected="">Choose...</option>
+                                                    <option value="1">Like</option>
+
+                                                </select>
+                                            </div>
+                                        </div>
+
+                                        <div class="form-group row">
+                                            <div class="col-10">
+                                                <h5 class="">CATEGORY</h5>
+                                                <select class="custom-select" id="inlineFormCustomSelect">
+                                                    <option selected="">Social</option>
+                                                    <option value="1">One</option>
+                                                    <option value="2">Two</option>
+                                                    <option value="3">Three</option>
+                                                </select>
+                                            </div>
+                                        </div>
+
+                                        <div class="form-group row">
+                                            <div class="col-sm-10 ">
+                                                <h5 class="">SOCIAL</h5>
+                                                <div style="margin-top:30px;">
+                                                    <div class="custom-control custom-checkbox social1">
+                                                        <input type="checkbox" class="custom-control-input" id="customCheck1">
+                                                        <label class="custom-control-label" for="customCheck1"><img src="assets/images/media/facebook.png"></label>
+                                                    </div>
+                                                    <div class="custom-control custom-checkbox social1">
+                                                        <input type="checkbox" class="custom-control-input" id="customCheck3">
+                                                        <label class="custom-control-label" for="customCheck2"><img src="assets/images/media/instagram.png"></label>
+                                                    </div>
+                                                    <div class="custom-control custom-checkbox social1">
+                                                        <input type="checkbox" class="custom-control-input" id="customCheck3">
+                                                        <label class="custom-control-label" for="customCheck3"><img src="assets/images/media/twitter.png"></label>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div class="form-group row">
+                                            <div class="col-sm-10">
+                                                <img src="assets/images/media/city.png">
+                                            </div>
+                                        </div>
+
+
+
+
+                                    </form>
+
+
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+
+
+
+                </div>
+            </div>
+        </div>
+
+
+
+
+
+
+        <!-- ============================================================== -->
+        <!-- End Page Content -->
+        <!-- ============================================================== -->
+
+
+    </div>
+    <!-- ============================================================== -->
+    <!-- End Container fluid  -->
+    <!-- ============================================================== -->
+</div>
+<!-- ============================================================== -->
+<!-- End Page wrapper  -->
+
+
+
+<?php
+include 'footer.php';
+?>
